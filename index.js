9fe0cfb1ae8b63c2c3092bd0a6a4d9aba092cf21
SanathRai <sanathrai03@gmail.com>
2025-08-20 00:11:43 +0530

    Bot implemented with start and jokes command

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-API_KEY='8226813914:AAFLNSbyN4AmNHAnDamp3duqEW1KqNG5UTQ'
+const TelegramBot = require('node-telegram-bot-api')
+const dotenv = require('dotenv')
+const axios = require('axios')
+
+dotenv.config()
+
+const Token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(Token, { polling: true});
+
+bot.on("message", (msg) => {
+  if (!msg.text.startsWith("/")) {
+    bot.sendMessage(msg.chat.id, "You said: " + msg.text);
+  }
+});
+
+bot.onText(/\/start/, (msg)=>{
+    bot.sendMessage(msg.chat.id, "Hello I am a bot. How can i help you?")
+})
+
+bot.onText(/\/jokes/, async (msg) => {
+  const chatId = msg.chat.id;
+  try {
+    const response = await axios.get("https://official-joke-api.appspot.com/random_joke");
+    const joke = `${response.data.setup}\n😂 ${response.data.punchline}`;
+    bot.sendMessage(chatId, joke);
+  } catch (error) {
+    console.error("Error fetching joke:", error.message);
+    bot.sendMessage(chatId, "❌ Sorry, I couldn’t fetch a joke right now.");
+  }
+});
